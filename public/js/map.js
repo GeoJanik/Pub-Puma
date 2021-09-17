74d35c4eba357a93cfe9687c01c9a3721d4aaaff
unknown <geojanik@gmail.com>
2021-09-17 13:49:00 +0200

    style et structure

---
+++ public/js/map.js
@@ -0,0 +1,24 @@
+class Carte {
+    constructor() {
+        this.lattitudeMap = 48.89555632603661;
+        this.longitudeMap = 2.2496143257227788;
+        this.conteneurMap = document.getElementById("map");
+        this.initMap();
+    }
+
+    initMap() {
+
+        this.map = L.map('map', {
+            zoomControl: false
+        }).setView([this.lattitudeMap, this.longitudeMap], 12);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+
+        /* CREATION DU MARKER SUR LA MAP */
+        const marker = L.marker([this.lattitudeMap, this.longitudeMap]);
+        this.map.addLayer(marker);
+    }
+}
+
+new Carte();
