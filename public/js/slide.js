50cda7c028b77c94e5e103711d672bd4fca8bbd3
Geoffrey JANIK <geo.janik@gmail.com>
2021-09-19 17:00:54 +0200

    indentation js

--- public/js/slide.js
+++ public/js/slide.js
@@ -4,39 +4,21 @@ class Slide {
         this.container = document.getElementById("container");
         this.video = document.getElementById('testVideo');
         console.log(this.video);
-        this.blockStyle();
-        this.timer();
-        this.son();
-
+        this.init();
     }
 
-
-    blockStyle() {
-        this.slide.addEventListener('click', () => {
-            this.container.style.transitionDuration = "1s"
-                this.container.style.transform =  "translate(0px, -208px)";
-                this.video.style.visibility = "hidden"; 
-            }) 
-        }
-               
-    blockStyleTimer() {
-                this.container.style.transitionDuration = "1s"
-                this.container.style.transform =  "translate(0px, -208px)";   
-                console.log(this.video);
-                this.video.style.visibility = "hidden";   
+    init() {
+        this.slide.addEventListener('click', () => this.blockStyleTimer());
+        setTimeout(() => this.hideVideo(), 20000);
+        this.video.addEventListener('click', ()=> this.video.muted = false);
+    }
+          
+    hideVideo() {
+        this.container.style.transitionDuration = "1s"
+        this.container.style.transform =  "translate(0px, -208px)";   
+        this.video.style.visibility = "hidden";   
     }        
         
-    timer() {
-        setTimeout(() => this.blockStyleTimer(), 20000)
-    }
-
-    son() {
-        this.video.addEventListener('click', ()=> {
-            this.video.muted = false;
-                })
-    }
-
-
 }
 
 new Slide();
