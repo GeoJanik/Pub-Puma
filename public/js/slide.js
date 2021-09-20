3cc9ebbb9a1799f3de8f06050ad3b9d458b595a4
Geoffrey JANIK <geo.janik@gmail.com>
2021-09-20 09:13:52 +0200

    test animation video js

--- public/js/slide.js
+++ public/js/slide.js
@@ -8,14 +8,17 @@ class Slide {
 
     init() {
         this.slide.addEventListener('click', () => this.hideVideo());
-        setTimeout(() => this.hideVideo(), 20000);
         this.video.addEventListener('click', ()=> this.video.muted = false);
+        setTimeout(() => this.hideVideo(), 20000);
+
     }
           
     hideVideo() {
         this.container.style.transitionDuration = "1s"
         this.container.style.transform =  "translate(0px, -208px)";   
-        this.video.style.visibility = "hidden";   
+        this.video.style.transform = "scale(0.1)";   
+        this.video.style.transition = "transform 1s ease-in-out";
+        
     }        
 }
 
